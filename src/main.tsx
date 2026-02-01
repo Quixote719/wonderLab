03629fd2079657d0b0fe1f82f6265f975d0f1ee1
Icarus <lwang7195@gmail.com>
2026-02-01 19:13:24 +0800

    feat: basic settings

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css'
-import App from './App.tsx'
+import ClockPage from '@/pages/clockPage.tsx'
+
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/clockPage" element={<ClockPage />} />
+        <Route path="/" element={<ClockPage />} />
+      </Routes>
+    </Router>
+  );
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
---
+++ src/pages/clockPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+
+const ClockPage = () => {
+    return (
+        <div>
+            <div className="text-4xl font-bold">Clock Page</div>
+            <Button variant="destructive">Button</Button>
+        </div>
+    );
+};
+
+export default ClockPage;
